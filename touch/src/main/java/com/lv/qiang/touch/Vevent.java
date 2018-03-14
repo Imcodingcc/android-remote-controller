59838e28b9797ee0c8c8a98a204045ec8c03d38f
lvqiang <lvqiang@leither.org>
2018-03-14 14:17:31 +0800

    delete odd parts

--- touch/src/main/java/com/lv/qiang/touch/Vevent.java
+++ touch/src/main/java/com/lv/qiang/touch/Vevent.java
@@ -6,18 +6,8 @@ import com.stericson.RootTools.RootTools;
 import com.stericson.RootTools.execution.CommandCapture;
 
 class Vevent {
-    private static final String TAG = Vevent.class.getName();
-    public void executeCommand(String command) throws Exception{
+    void executeCommand(String command) throws Exception{
         CommandCapture cmd = new CommandCapture(0, command);
         RootTools.getShell(true).add(cmd);
-    }
-    @SuppressLint("DefaultLocale")
-    void sendevent(String event_num, int param_1, int param_2, int param_3) {
-        try {
-            String cmd = String.format("sendevent /dev/input/%s %d %d %d", event_num, param_1, param_2, param_3);
-            executeCommand(cmd);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
